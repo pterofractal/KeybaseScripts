9c976c33f2cec976ad315f833029475a7dd6734f
Krishna Sivaranjan <ksivaran@amazon.com>
2014-08-29 12:39:58 -0700

    Adding comment remving plist file

--- decryptMessages.py
+++ decryptMessages.py
@@ -10,7 +10,9 @@ from sys import stdin
 from os import listdir
 from os.path import isfile, join
 
+# Set this to your homedir. For some reason os.environ['USER'] didn't seem to work. Not sure why yet.
 homedir='/Users/ksivaran/'
+
 syslog.openlog("DecryptMessagesLog")
 syslog.syslog(syslog.LOG_ALERT, "Started Script")
 
