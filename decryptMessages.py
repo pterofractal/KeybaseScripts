510d9bafcc9fcae8ca42e48e0809b2b7cf301a83
Krishna Sivaranjan <ksivaran@amazon.com>
2014-08-29 12:36:06 -0700

    Revamping apple script so it is more of a passthru to a python script Wrote a python script to decrypt messages in a folder no longer need to set passphrase.

---
+++ decryptMessages.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import argparse
+import datetime
+import os
+import subprocess
+import sys
+import syslog
+from sys import stdin
+from os import listdir
+from os.path import isfile, join
+
+homedir='/Users/ksivaran/'
+syslog.openlog("DecryptMessagesLog")
+syslog.syslog(syslog.LOG_ALERT, "Started Script")
+
+parser = argparse.ArgumentParser(description='Decrypts messages found in a single folder')
+parser.add_argument('folder', help='the folder with encrypted messages')
+parser.add_argument('decryptFolder', help='the folder store decrypted messages in')
+args = parser.parse_args()
+
+for f in listdir(args.folder):
+	filePath = join(args.folder,f)
+	if isfile(filePath) and f.endswith('.txt'):
+		decryptCommand = 'keybase --homedir %s decrypt %s' % (homedir, filePath)
+		syslog.syslog(syslog.LOG_ALERT, decryptCommand)
+		subprocess.call('%s > %s ' % (decryptCommand, join(args.decryptFolder, f)), shell=True)
+		syslog.syslog(syslog.LOG_ALERT, "Decrypted message. Trying to delete")
+		os.remove(filePath)
+
+print 'Decrypted messages'
